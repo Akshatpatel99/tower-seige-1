dc7d3d5d31e3220d81cfff7221e1e5f80de87a20
Akshat <78414713+Akshatpatel99@users.noreply.github.com>
2021-05-10 12:30:36 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,23 @@
+class Block{
+    constructor(x, y, width, height, angle) {
+        var options = {
+          restitution : 0.7,
+          friction : 0.6,
+          isStatic : false,
+        }
+              this.body = Bodies.rectangle(x, y, width, height, options);
+              this.width = width;
+              this.height = height;
+              World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+
+        rectMode(CENTER);
+        rect(0, 0, this.width, this.height);
+        pop();
+      }
+}
